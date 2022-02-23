b963473d71922bd783084786888995a97b488cd4
lzzzy <s.bey@t-online.de>
2022-02-23 23:32:46 +0100

    kleinste polygone werden zuerst aufgedeckt

--- DataType.py
+++ DataType.py
@@ -8,6 +8,9 @@ import numpy as np
 
 from PIL import Image, ImageTk
 
+from shapely.geometry import Polygon, box
+import operator
+
 class ImageController:
     valid_images = [".jpg",".gif",".png",".tga"]
     imageFiles = []
@@ -38,49 +41,79 @@ class ImageController:
         else:
             return img.resize((max_width, int(max_width/ratio)), Image.ANTIALIAS)
 
-    def next_image(self):
-        self.canvas.delete(self.tiles)
-        self.tiles.clear()
+    def canvas_create_image(self):
         pil_img = Image.open(self.imageFiles[self.imageFilesIndex])
         self.imageFilesIndex += 1
         pil_img = self.resize(pil_img, self.root.winfo_screenwidth(), self.root.winfo_height())
         self.imgage = ImageTk.PhotoImage(pil_img)
-
         width = self.imgage.width()
         height = self.imgage.height()
         self.canvas.config(width=width, height=height)
-
         self.canvas.create_image((0, 0), anchor=tk.NW, image=self.imgage)
-    
-        # return
+        return width, height
 
+    def canvas_create_tiles(self, width, height):
         points = [(random.randint(0,width),random.randint(0,height)) for i in range(9)]
         # add 4 distant dummy points
         points = np.append(points, [[width*99,height*99], [-width*99,height*99], [width*99,-height*99], [-width*99,-height*99]], axis = 0)
         vor = Voronoi(points)
 
-        polygons = {}
-        for id, region_index in enumerate(vor.point_region):
+        polygons = []
+        for region_index in vor.point_region:
             points = []
             for vertex_index in vor.regions[region_index]:
                 if vertex_index != -1:  # the library uses this for infinity
                     points.append(list(vor.vertices[vertex_index]))
             points.append(points[0])
-            polygons[id]=points
+            polygons.append(points)
 
-        for id, poly in enumerate(polygons.values()):
+        # remove polygons for dummy point
+        for i in range(4):
+            polygons.pop()
+
+        for poly in polygons:
             self.tiles.append(
-                self.canvas.create_polygon(
-                    *poly, 
-                    fill="#"+("%06x"%random.randint(0,16777215)),
-                    outline='black',
-                    width=1
+                Tile(
+                    poly,
+                    width,
+                    height,
+                    self.canvas.create_polygon(
+                        *poly, 
+                        fill="#"+("%06x"%random.randint(0,16777215)),
+                        outline='black',
+                        width=1
+                    )
                 )
             )
-        # remove dummy polygons
-        for i in range(4):
-            self.tiles.pop()
+        keyfun= operator.attrgetter("area")
+        print(self.tiles)
+        self.tiles.sort(key=keyfun, reverse=True)
+        print(self.tiles)
+
+    def canvas_delete_tiles(self):
+        for tile in self.tiles:
+            self.canvas.delete(tile.canvas_poly)
+        self.tiles.clear()
+
+    def next_image(self):
+        self.canvas_delete_tiles()
+        width, height = self.canvas_create_image()
+        self.canvas_create_tiles(width, height)
 
     def next_tile(self):
-        self.canvas.delete(self.tiles.pop())
+        self.canvas.delete(self.tiles.pop().canvas_poly)
             
+class Tile:
+    canvas_poly = None
+    area = None
+        
+    def __init__(self, poly, width, height,canvas_poly):
+        self.canvas_poly = canvas_poly
+        pgon = Polygon(poly)
+        self.area = pgon.intersection(box(0, 0, width, height)).area
+
+    def __str__(self):
+        return str(self.area)
+
+    def __repr__(self):
+        return self.__str__()
